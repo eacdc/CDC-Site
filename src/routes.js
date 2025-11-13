5bc2f079dd6c2f2de44f9d911aee1dbeb396ac03
eacdc <ea@cdcprinters.com>
2025-11-13 16:36:39 +0530

    Add machine floor dashboard UI

--- src/routes.js
+++ src/routes.js
@@ -1677,19 +1677,41 @@ router.post('/grn/barcode-status', async (req, res) => {
         request.input('BarcodeNo', sql.Int, barcodeNum);
 
         const query = `
-            SELECT Category, EventDate, JobBookingNo
-            FROM (
-                select 'packing-slip' as Category, p.datetime, b.JobBookingNo from PackingSlipBarcodeEntry p inner join jobbookingjobcard b on p.JobBookingID=b.JobBookingID where barcodeno = 100067
-UNION
-select 'GPN' as Category, p.CreatedDate, b.JobBookingNo from FinishGoodsTransactionDetail p inner join jobbookingjobcard b on p.JobBookingID=b.JobBookingID 
-	inner join FinishGoodsTransactionMain p2 on p.FGTransactionID=p2.FGTransactionID where barcode = 100067 and ParentFGTransactionID=0 --gpn
-	and isnull(p.isdeletedtransaction,0)=0 and isnull(p2.isdeletedtransaction,0)=0
-UNION
-select 'Delivery Note' as Category, p.CreatedDate, b.JobBookingNo from FinishGoodsTransactionDetail p inner join jobbookingjobcard b on p.JobBookingID=b.JobBookingID 
-	inner join FinishGoodsTransactionMain p2 on p.FGTransactionID=p2.FGTransactionID where barcode = 100067 and ParentFGTransactionID>0 --dn
-	and isnull(p.isdeletedtransaction,0)=0 and isnull(p2.isdeletedtransaction,0)=0
-            ) AS Combined
-            ORDER BY EventDate ASC, Category ASC
+            SELECT
+                'packing-slip' AS Category,
+                p.[DateTime] AS EventDate,
+                b.JobBookingNo
+            FROM PackingSlipBarcodeEntry AS p
+            INNER JOIN jobbookingjobcard AS b ON p.JobBookingID = b.JobBookingID
+            WHERE p.BarcodeNo = @BarcodeNo
+
+            UNION
+
+            SELECT
+                'GPN' AS Category,
+                p.CreatedDate AS EventDate,
+                b.JobBookingNo
+            FROM FinishGoodsTransactionDetail AS p
+            INNER JOIN jobbookingjobcard AS b ON p.JobBookingID = b.JobBookingID
+            INNER JOIN FinishGoodsTransactionMain AS p2 ON p.FGTransactionID = p2.FGTransactionID
+            WHERE p.Barcode = @BarcodeNo
+              AND ISNULL(p.ParentFGTransactionID, 0) = 0
+              AND ISNULL(p.IsDeletedTransaction, 0) = 0
+              AND ISNULL(p2.IsDeletedTransaction, 0) = 0
+
+            UNION
+
+            SELECT
+                'Delivery Note' AS Category,
+                p.CreatedDate AS EventDate,
+                b.JobBookingNo
+            FROM FinishGoodsTransactionDetail AS p
+            INNER JOIN jobbookingjobcard AS b ON p.JobBookingID = b.JobBookingID
+            INNER JOIN FinishGoodsTransactionMain AS p2 ON p.FGTransactionID = p2.FGTransactionID
+            WHERE p.Barcode = @BarcodeNo
+              AND ISNULL(p.ParentFGTransactionID, 0) > 0
+              AND ISNULL(p.IsDeletedTransaction, 0) = 0
+              AND ISNULL(p2.IsDeletedTransaction, 0) = 0
         `;
 
         const result = await request.query(query);
