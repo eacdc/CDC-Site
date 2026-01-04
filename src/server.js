c693dc18b17ebb98c55dc2f2340e9f9f2b5a64bb
eacdc <ea@cdcprinters.com>
2026-01-04 12:10:37 +0530

    change

--- src/server.js
+++ src/server.js
@@ -25,7 +25,8 @@ app.use(cors({
 	credentials: true
 }));
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 // Simple console request logger for visibility
 app.use((req, res, next) => {
 	const start = Date.now();
